edca11fd382355db7cc8c812d91fc9aa332f3fdb
DESKTOP-RFQ9V3C\take <goozooloo123@gmail.com>
2023-03-26 12:02:07 +0900

    Boardモデルの作成、index/show/new/create/edit/updateアクションの追加

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,52 @@
+class BoardsController < ApplicationController
+    # before_action :authenticate_user!, only: []
+
+    def index
+        @boards = Board.all
+        # render 'boards/index'
+    end
+
+    def show
+        @board = Board.find(params[:id])
+    end
+
+    def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+            redirect_to board_path(@board), notice: '保存できました'
+        else
+            flash.now[:error] = '保存に失敗しました'
+            render :new
+        end
+    end
+
+    def edit
+        @board = current_user.boards.find(params[:id])
+    end
+
+    def update
+        @board = current_user.boards.find(params[:id])
+        if @board.update(board_params)
+            redirect_to board_path(@board), notice: '更新できました'
+        else
+            flash.now[:error] = '更新に失敗しました'
+            render :edit
+        end
+    end
+
+    
+
+    private
+    def board_params
+        params.require(:board).permit(:title, :content)
+    end
+
+    def set_board
+        @board = Board.find(params[:id])
+    end
+
+end
---
+++ app/models/board.rb
@@ -0,0 +1,9 @@
+class Board < ApplicationRecord
+    # before_action :authenticate_user!
+
+    belongs_to :user
+
+    def author_name
+        user.display_name
+    end
+end
